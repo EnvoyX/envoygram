c81b985a281770d1fc54fcbc35232a551191c3d5
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-02-27 15:19:38 +0700

    update: created navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-        <SignUpButton mode="modal">
-          <Button variant={"ghost"}>Sign up</Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle></ModeToggle>
+      <h1>Home Page</h1>
     </div>
   );
 }
