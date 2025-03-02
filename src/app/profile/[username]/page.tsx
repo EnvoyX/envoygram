2816527169f80c9b8a18256b1fc62e0075402cef
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-03-02 18:54:05 +0700

    update: view profile page feature in progress

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,3 +1,18 @@
+import {getProfileByUsername} from "@/actions/profile.action";
+
+
+// Update Metadata
+export const generateMetadata = async ({params}: { params: { username: string } }) => {
+    const user = await getProfileByUsername(params.username)
+    if (!user) return;
+
+    return {
+        title: `${user.name ?? user.username}`,
+        description: user.bio || `Check out ${user.username}'s profile`,
+    }
+}
+
+
 function ProfilePage({params}: { params: { username: string } }) {
     console.log("Params:", params);
     return <div>Profile Page</div>
